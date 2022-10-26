932727857ad5d1a65991d7d1d8f96ed9eef1f1c9
Alex <aleksandaredochev@gmail.com>
2022-10-26 18:08:55 +0300

    more tasks

---
+++ src/BasicSyntax/code/Exerices/P06StrongNumber.java
@@ -0,0 +1,11 @@
+package BasicSyntax.code.Exerices;
+
+import java.util.Scanner;
+
+public class P06StrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int xx = Integer.parseInt(scanner.nextLine());
+        String xxx = scanner.nextLine();
+    }
+}
---
+++ src/BasicSyntax/code/codePresets.java
@@ -0,0 +1,12 @@
+package BasicSyntax.code;
+
+import java.util.Scanner;
+
+public class codePresets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int xx = Integer.parseInt(scanner.nextLine());
+        String xxx = scanner.nextLine();
+
+    }
+}
