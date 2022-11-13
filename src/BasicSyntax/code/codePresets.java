f49a076770c76a2f3ce40992315193c38e662077
Alex <aleksandaredochev@gmail.com>
2022-11-13 22:35:44 +0200

    more tasks 13 nov

--- src/BasicSyntax/code/codePresets.java
+++ src/BasicSyntax/code/codePresets.java
@@ -7,6 +7,7 @@ public class codePresets {
         Scanner scanner = new Scanner(System.in);
         int xx = Integer.parseInt(scanner.nextLine());
         String xxx = scanner.nextLine();
+        double x = Double.parseDouble(scanner.nextLine());
 
     }
 }
